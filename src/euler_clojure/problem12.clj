83be9b841f027c7ccf9bb50e46cf8167884dd6bb
tetigi <luke.tomlin@gmail.com>
2018-03-28 22:11:57 +0100

    Solved problem 12

---
+++ src/euler_clojure/problem12.clj
@@ -0,0 +1,33 @@
+(ns euler-clojure.problem12)
+
+(defn is-prime
+  [n]
+  (if (<= n 1)
+    false
+    (if (<= n 3)
+      true
+      (if (or (= 0 (mod n 2)) (= 0 (mod n 3)))
+        false
+        (if (keep #(or (= 0 (mod n %)) (= 0 (mod n (+ % 2)))) (range 5 (inc (int (Math/sqrt n))) 6)) true false)))))
+
+(defn prime-factors
+  [n]
+  (cond
+    (= 0 (mod n 2)) (conj (prime-factors (quot n 2)) 2)
+    :else (loop [i 3 n' n factors []]
+      (cond
+        (= 0 (mod n' i)) (recur i (quot n' i) (conj factors i))
+        (>= i (int (Math/sqrt n'))) (cond-> factors (is-prime n') (conj n'))
+        :else (recur (+ i 2) n' factors)))))
+
+(defn triangle-nums
+  []
+  (reductions + (rest (range))))
+
+(defn num-divisors
+  [x]
+  (apply * (map inc (vals (frequencies (prime-factors x))))))
+
+(defn solve
+  [limit]
+  (first (drop-while #(< (num-divisors %) limit) (triangle-nums))))
