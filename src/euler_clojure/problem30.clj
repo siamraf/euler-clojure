aae7481c83444e2edf7a68b81cc9fe54d3001eb0
Luke Tomlin <luke.tomlin@gmail.com>
2018-04-12 10:40:13 +0100

    Solved problem 30

--- src/euler_clojure/problem30.clj
+++ src/euler_clojure/problem30.clj
@@ -1,1 +1,9 @@
 (ns euler-clojure.problem30)
+
+(defn is-power-of-digits
+  [power n]
+  (= n (apply + (map #(int (Math/pow (Character/getNumericValue %) power)) (str n)))))
+
+(defn solve
+  [n]
+  (filter (partial is-power-of-digits n) (map (partial + 10) (range))))
